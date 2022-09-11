32b72279748d599e897eddb728c88923a8038d87
Tejal Gaikwad <you@example.com>
2022-09-11 15:28:29 +0530

    Added : Check equality of two line

--- LineComparison.java
+++ LineComparison.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class LineComparison {
 
 	public static void main(String[] args) {
-
+		
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Enter the Co-ordinates of x1 and y1 : ");
@@ -14,8 +14,21 @@ public class LineComparison {
 		System.out.println("Enter the Co-ordinates of x2 and y2 : ");
 		int x2 = sc.nextInt();
 		int y2 = sc.nextInt();
-		double length = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
-		System.out.println("Length of the given Line is : " + length + " units");
+		Double len1 = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+		System.out.println("Enter the Co-ordinates of x3 and y3 : ");
+		int x3 = sc.nextInt();
+		int y3 = sc.nextInt();
+		System.out.println("Enter the Co-ordinates of x4 and y4 : ");
+		int x4 = sc.nextInt();
+		int y4 = sc.nextInt();
+		Double len2 = Math.sqrt((x4 - x3) ^ 2 + (y4 - y3) ^ 2);
+		System.out.println("Length of the first Line is : " + len1 + " units");
+		System.out.println("Length of the second Line is : " + len2 + " units");
+		System.out.println();
+		if (len1.equals(len2))
+			System.out.println("Both lines are equal ");
+		else
+			System.out.println("Both lines are Not equal ");
+
 	}
-	
 }
