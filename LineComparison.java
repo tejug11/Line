ceb2aca505af7dceb7059d3581b787750b449c48
Tejal Gaikwad <you@example.com>
2022-09-11 15:06:05 +0530

    Added : Length of 2 points

--- LineComparison.java
+++ LineComparison.java
@@ -1,7 +1,21 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class LineComparison {
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to line comparison computation program");
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the Co-ordinates of x1 and y1 : ");
+		int x1 = sc.nextInt();
+		int y1 = sc.nextInt();
+		System.out.println("Enter the Co-ordinates of x2 and y2 : ");
+		int x2 = sc.nextInt();
+		int y2 = sc.nextInt();
+		double length = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+		System.out.println("Length of the given Line is : " + length + " units");
 	}
+	
 }
