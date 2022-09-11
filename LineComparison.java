2ecbc93abb7a3857beb4d9c7e6374535add83f7f
Tejal Gaikwad <you@example.com>
2022-09-11 15:43:55 +0530

    Added : code to compare the length of both lines

--- LineComparison.java
+++ LineComparison.java
@@ -5,30 +5,38 @@ import java.util.Scanner;
 public class LineComparison {
 
 	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
 
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Enter the Co-ordinates of 1st line ");
+		
 		System.out.println("Enter the Co-ordinates of x1 and y1 : ");
-		int x1 = sc.nextInt();
-		int y1 = sc.nextInt();
+		int x1 = in.nextInt();
+		int y1 = in.nextInt();
 		System.out.println("Enter the Co-ordinates of x2 and y2 : ");
-		int x2 = sc.nextInt();
-		int y2 = sc.nextInt();
+		int x2 = in.nextInt();
+		int y2 = in.nextInt();
 		Double len1 = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+		System.out.println();
+		
+		System.out.println("Enter the Co-ordinates of 2nd line ");
+		
 		System.out.println("Enter the Co-ordinates of x3 and y3 : ");
-		int x3 = sc.nextInt();
-		int y3 = sc.nextInt();
+		int x3 = in.nextInt();
+		int y3 = in.nextInt();
 		System.out.println("Enter the Co-ordinates of x4 and y4 : ");
-		int x4 = sc.nextInt();
-		int y4 = sc.nextInt();
+		int x4 = in.nextInt();
+		int y4 = in.nextInt();
 		Double len2 = Math.sqrt((x4 - x3) ^ 2 + (y4 - y3) ^ 2);
 		System.out.println("Length of the first Line is : " + len1 + " units");
 		System.out.println("Length of the second Line is : " + len2 + " units");
 		System.out.println();
-		if (len1.equals(len2))
-			System.out.println("Both lines are equal ");
+		if (len1.compareTo(len2) == 1)
+			System.out.println("Length of Line 1 is greater than Line 2 ");
+		else if (len1.compareTo(len2) == -1)
+			System.out.println("Length of Line 1 is less than Line 2 ");
 		else
-			System.out.println("Both lines are Not equal ");
+			System.out.println("Both lines are equal in length");
 
 	}
 }
